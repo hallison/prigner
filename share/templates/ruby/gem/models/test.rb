32147bb922294fea5b8cd90cba84d47a903cdba0
Hallison Batista <hallison@codigorama.com>
2010-11-25 08:47:48 -0400

    Fixes in the templates that includes test

---
+++ share/templates/ruby/gem/models/test.rb
@@ -0,0 +1,16 @@
+require "test/unit"
+require "test/helper"
+require "lib/<%=project.name%>"
+
+class <%=project.class_name%>Test < Test::Unit::TestCase
+
+  def setup
+    # start here
+  end
+
+  should "check the truth" do
+    assert true
+  end
+
+end
+
